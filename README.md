b788f0d50d12de26ed7a6a9a8519ed7e5382f3c7
Hank Beaver <dev+hank@blinqmedia.com>
2014-04-30 18:05:10 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-server_density_sidekiq_queues
+SidekiqQueues
 =============================
 
-Simple Server Density plugin to show sizes of your Sidekiq queues
+[Sidekiq](https://github.com/mperham/sidekiq) plugin for ServerDensity to show queue sizes. 
+
+#Metrics
+
+The plugin will report one metric per queue in Sidekiq.
+
+##Assumptions
+
+* Requires redis-cli in PATH.
+* Presumes installation on the primary Redis server.
+* Only tested for Linux.
+
+##Installation
+
+Copy `SidekiqQueues.py` plugin to your `sd-agent` plugins folder (`/usr/bin/sd-agent/plugins`). Create the plugins folder if it doesn't exist.
+
+
+#Thanks
+Drew configuration inspiration from https://github.com/justincase/sd-beanstalkd/blob/master/Beanstalkd.py.
---
+++ SidekiqQueues.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import re
+import commands
+
+SIDEKIQ_QUEUES = "redis-cli --raw smembers sidekiq:queues"
+REDIS_AVAIL = int(commands.getoutput("type redis-cli > /dev/null 2>&1 ; echo $?")) == 0
+
+class SidekiqQueue:
+    def __init__(self, agent_config, checks_logger, raw_config):
+        self.agent_config = agent_config
+        self.checks_logger = checks_logger
+        self.raw_config = raw_config
+
+        if self.agentConfig is None:
+            self.set_default_config()
+
+        if ('Sidekiq' not in self.agentConfig):
+            self.set_default_config()
+
+    def set_default_config(self):
+        self.agentConfig = {}
+        self.agentConfig['Sidekiq'] = {'namespace': 'sidekiq'}
+
+    def run(self):
+        stats = {}
+        #only run redis-cli commands on servers that have it, right now the app master
+        if REDIS_AVAIL:
+            namespace = agentConfig['Sidekiq']['namespace']
+            command = "redis-cli --raw llen %(namespace)s:queue:" % locals()
+            for queue in commands.getoutput(SIDEKIQ_QUEUES).splitlines():
+                stats[queue] = int(commands.getoutput(command+queue))
+        return stats
+
+if __name__ == '__main__':
+    sidekiq = SidekiqQueue(None, None, None)
+    print sidekiq.run()
+
